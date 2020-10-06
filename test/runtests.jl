7f302d01198479398047625887cc8a8fc319c97f
tomroesch <troeschi@caltech.edu>
2020-10-06 16:14:38 -0700

    fixed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Jevo, Test, BenchmarkTools, LinearAlgebra
 @testset "Initiating " begin
     @testset "Simple binding sites" begin
 
-        pop = binding_sites(N=4, l=10, n=4)
+        pop = Jevo.binding_sites(N=4, l=10, n=4)
         initiate!(pop, 3)
         # Test population size
         @test sum(pop.freqs) == pop.N
@@ -12,16 +12,16 @@ using Jevo, Test, BenchmarkTools, LinearAlgebra
         # Test sequence length of species
         @test length.(pop.seqs) == (ones(Int64, 3) .* pop.L)
         # Test warning message
-        @test_logs (:warn, "l>L. Choosing L=l.") binding_sites(N=4, l=10, L=2, n=4)
-        @test_nowarn initiate!(pop, overwrite=true)
+        @test_logs (:warn, "l>L. Choosing L=l.") Jevo.binding_sites(N=4, l=10, L=2, n=4)
+        @test_nowarn Jevo.initiate!(pop, overwrite=true)
 
     end
 
     @testset "Driver Trailer sites" begin
         @testset "Random sequences" begin
 
-            pop1 = driver_trailer(N=4, l=10, n=4, m=4)
-            initiate!(pop1, 3)
+            pop1 = Jevo.driver_trailer(N=4, l=10, n=4, m=4)
+            Jevo.initiate!(pop1, 3)
             # Test population size
             @test sum(pop1.freqs) == pop1.N
             # Test number of species
@@ -30,14 +30,14 @@ using Jevo, Test, BenchmarkTools, LinearAlgebra
             @test length.(pop1.seqs) == (ones(Int64, 3) .* pop1.L)
             # Test length of driver
             @test length(pop1.driver) == pop1.L
-            @test_nowarn initiate!(pop1, overwrite=true)
+            @test_nowarn Jevo.initiate!(pop1, overwrite=true)
 
         end
 
         @testset "Optimal Sequence" begin
 
-            pop = driver_trailer(N=4, l=10, n=4, m=4)
-            initiate!(pop, 3, opt=true)
+            pop = Jevo.driver_trailer(N=4, l=10, n=4, m=4)
+            Jevo.initiate!(pop, 3, opt=true)
             @test length(pop.seqs) == 1
             @test pop.seqs[1] == pop.driver
             @test length(pop.freqs) == 1
@@ -47,20 +47,20 @@ using Jevo, Test, BenchmarkTools, LinearAlgebra
 
         @testset "Given Driver" begin
 
-            pop2 = driver_trailer(N=4, l=10, n=4, m=4)
-            initiate!(pop2, 3, driver=[1, 2, 3, 4])
+            pop2 = Jevo.driver_trailer(N=4, l=10, n=4, m=4)
+            Jevo.initiate!(pop2, 3, driver=[1, 2, 3, 4])
             # Test that partial driver sequence is incorporated
             @test pop2.driver[1:4] == [1, 2, 3, 4]
-            pop3 = driver_trailer(N=4, L=10, n=4, m=12)
-            initiate!(pop3, 3, driver=collect(1:12))
+            pop3 = Jevo.driver_trailer(N=4, L=10, n=4, m=12)
+            Jevo.initiate!(pop3, 3, driver=collect(1:12))
             # Test that driver too long is cut short
             @test pop3.driver == collect(1:10)
             # Test warning message
-            @test_logs (:warn, "l>L. Choosing L=l.") driver_trailer(N=4, l=10, L=2, n=4)
+            @test_logs (:warn, "l>L. Choosing L=l.") Jevo.driver_trailer(N=4, l=10, L=2, n=4)
 
-            pop4 = driver_trailer(N=4, L=10, n=4, m=4)
+            pop4 = Jevo.driver_trailer(N=4, L=10, n=4, m=4)
             # Test error message for wrong driver sequence
-            @test_throws ArgumentError initiate!(pop4, 3, driver=collect(1:12))
+            @test_throws ArgumentError Jevo.initiate!(pop4, 3, driver=collect(1:12))
 
         end
     end
@@ -68,8 +68,8 @@ using Jevo, Test, BenchmarkTools, LinearAlgebra
     @testset "Driver Trailer sites variable length" begin
         @testset "Random Sequences" begin
 
-            pop1 = driver_trailer_l(N=4, l_0=10, n=4, m=4, L=20)
-            initiate!(pop1, 3)
+            pop1 = Jevo.driver_trailer_l(N=4, l_0=10, n=4, m=4, L=20)
+            Jevo.initiate!(pop1, 3)
             # Test population size
             @test sum(pop1.freqs) == pop1.N
             # Test number of species
@@ -86,8 +86,8 @@ using Jevo, Test, BenchmarkTools, LinearAlgebra
 
         @testset "Optimal Sequence" begin
 
-            pop = driver_trailer_l(N=4, l_0=10, n=4, m=4, L=20)
-            initiate!(pop, 3, opt=true)
+            pop = Jevo.driver_trailer_l(N=4, l_0=10, n=4, m=4, L=20)
+            Jevo.initiate!(pop, 3, opt=true)
             @test length(pop.seqs) == 1
             @test pop.seqs[1] == pop.driver
             @test length(pop.freqs) == 1
@@ -97,36 +97,36 @@ using Jevo, Test, BenchmarkTools, LinearAlgebra
         end
 
         @testset "Given Trailer" begin
-            pop2 = driver_trailer_l(N=4, l_0=10, n=4, m=4)
-            initiate!(pop2, 3, driver=[1, 2, 3, 4])
+            pop2 = Jevo.driver_trailer_l(N=4, l_0=10, n=4, m=4)
+            Jevo.initiate!(pop2, 3, driver=[1, 2, 3, 4])
             # Test that partial driver sequence is incorporated
             @test pop2.driver[1:4] == [1, 2, 3, 4]
 
-            pop3 = driver_trailer_l(N=4, L=10, n=4, m=12)
-            initiate!(pop3, 3, driver=collect(1:12))
+            pop3 = Jevo.driver_trailer_l(N=4, L=10, n=4, m=12)
+            Jevo.initiate!(pop3, 3, driver=collect(1:12))
             # Test that driver too long is cut short
             @test pop3.driver == collect(1:10)
             # Test warning message
-            @test_logs (:warn, "l_0>L. Choosing L=l_0.") driver_trailer_l(N=4, l_0=10, L=2, n=4)
+            @test_logs (:warn, "l_0>L. Choosing L=l_0.") Jevo.driver_trailer_l(N=4, l_0=10, L=2, n=4)
 
-            pop4 = driver_trailer_l(N=4, L=10, n=4, m=4)
+            pop4 = Jevo.driver_trailer_l(N=4, L=10, n=4, m=4)
             # Test error message for wrong driver sequence
-            @test_throws ArgumentError initiate!(pop4, 3, driver=collect(1:12))
+            @test_throws ArgumentError Jevo.initiate!(pop4, 3, driver=collect(1:12))
         end
     end
 
     @testset "Monomorphic Population" begin
 
         @testset "Initiate Random" begin
-            pop = mono_pop(N=10, l=10, n=4, m=4)
-            initiate!(pop)
+            pop = Jevo.mono_pop(N=10, l=10, n=4, m=4)
+            Jevo.initiate!(pop)
             @test length(pop.seqs) == 10
             @test length(pop.driver) == 10
         end
 
         @testset "Initiate Optimal" begin
-            pop = mono_pop(N=10, l=10, n=4, m=4)
-            initiate!(pop, opt=true)
+            pop = Jevo.mono_pop(N=10, l=10, n=4, m=4)
+            Jevo.initiate!(pop, opt=true)
             @test length(pop.seqs) == 10
             @test length(pop.driver) == 10
             @test pop.seqs == pop.driver
@@ -138,65 +138,65 @@ end
 
 @testset "Mutations" begin
     @testset "Find existing sequences" begin
-        pop = binding_sites(N=8, L=1, n=4, seqs=[[1], [2], [3], [4]], freqs=[2, 2, 2, 2])
-        mutation!(pop)
+        pop = Jevo.binding_sites(N=8, L=1, n=4, seqs=[[1], [2], [3], [4]], freqs=[2, 2, 2, 2])
+        Jevo.mutation!(pop)
         # Test that mutation to existing species does not add new species
         @test length(pop.seqs) == 4
         @test length(pop.freqs) == 4
     end
     @testset "Add new species" begin
-        pop = binding_sites(N=2, L=1, n=2)
-        initiate!(pop, 1)
-        mutation!(pop)
+        pop = Jevo.binding_sites(N=2, L=1, n=2)
+        Jevo.initiate!(pop, 1)
+        Jevo.mutation!(pop)
         # Test that new species is created
         @test length(pop.seqs) == 2
         @test length(pop.freqs) == 2
     end
     @testset "Driver mutation" begin
-        pop = driver_trailer(N=4, L=10, n=4, m=12)
-        initiate!(pop, 1, driver=collect(1:12))
-        mutation!(pop)
+        pop = Jevo.driver_trailer(N=4, L=10, n=4, m=12)
+        Jevo.initiate!(pop, 1, driver=collect(1:12))
+        Jevo.mutation!(pop)
         # Test that new species is created
         @test length(pop.seqs) == 2
         @test length(pop.freqs) == 2
 
-        pop2 = driver_trailer(N=4, L=10, n=4, m=4)
-        initiate!(pop2, 3)
+        pop2 = Jevo.driver_trailer(N=4, L=10, n=4, m=4)
+        Jevo.initiate!(pop2, 3)
         tmp_driver = copy(pop2.driver)
-        driver_mutation!(pop2)
+        Jevo.driver_mutation!(pop2)
         # Test that driver sequence is mutated
         @test tmp_driver != pop2.driver
         @test sum(tmp_driver .!= pop2.driver) == 1
     end
     @testset "Length mutation" begin
-        pop = driver_trailer_l(N=4, L=10, n=4, m=12)
-        initiate!(pop, 1, driver=collect(1:12))
-        mutation!(pop)
+        pop = Jevo.driver_trailer_l(N=4, L=10, n=4, m=12)
+        Jevo.initiate!(pop, 1, driver=collect(1:12))
+        Jevo.mutation!(pop)
         # Test that new species is created
         @test length(pop.seqs) == 2
         @test length(pop.freqs) == 2
         @test length(pop.l) == 2
 
-        pop2 = driver_trailer_l(N=4, L=10, n=4, m=4)
-        initiate!(pop2, 3)
+        pop2 = Jevo.driver_trailer_l(N=4, L=10, n=4, m=4)
+        Jevo.initiate!(pop2, 3)
         tmp_driver = copy(pop2.driver)
-        driver_mutation!(pop2)
+        Jevo.driver_mutation!(pop2)
         # Test that driver sequence is mutated
         @test tmp_driver != pop2.driver
         @test sum(tmp_driver .!= pop2.driver) == 1
 
-        pop3 = driver_trailer_l(N=1, L=10, n=4, m=4)
-        initiate!(pop3, 1)
-        mutation!(pop3)
+        pop3 = Jevo.driver_trailer_l(N=1, L=10, n=4, m=4)
+        Jevo.initiate!(pop3, 1)
+        Jevo.mutation!(pop3)
         # Test that new species is created
         @test length(pop3.seqs) == 1
         @test length(pop3.freqs) == 1
         @test length(pop3.l) == 1
 
 
-        pop4 = driver_trailer_l(N=1, L=10, n=4, m=4)
-        initiate!(pop4, 1)
-        length_mutation!(pop4)
+        pop4 = Jevo.driver_trailer_l(N=1, L=10, n=4, m=4)
+        Jevo.initiate!(pop4, 1)
+        Jevo.length_mutation!(pop4)
         # Test that new species is created
         @test length(pop4.seqs) == 1
         @test length(pop4.freqs) == 1
@@ -205,18 +205,18 @@ end
 end
 
 @testset "Substitutions" begin
-    pop = driver_trailer(N=4, l=10, n=4, m=4)
-    initiate!(pop, 2)
+    pop = Jevo.driver_trailer(N=4, l=10, n=4, m=4)
+    Jevo.initiate!(pop, 2)
     emat = Matrix{Float64}(I, 4, 4)
-    f = fermi_fitness(l=10, beta=1, f0=1, fl=0)
+    f = Jevo.fermi_fitness(l=10, beta=1, f0=1, fl=0)
     # Test error for too many species
-    @test_throws ArgumentError bp_substitution!(pop, emat, f)
+    @test_throws ArgumentError Jevo.bp_substitution!(pop, emat, f)
 
-    pop = driver_trailer(N=4, l=10, n=4, m=4)
-    initiate!(pop, 1)
+    pop = Jevo.driver_trailer(N=4, l=10, n=4, m=4)
+    Jevo.initiate!(pop, 1)
     temp_seqs = deepcopy(pop.seqs)
 
-    bp_substitution!(pop, emat, f)
+    Jevo.bp_substitution!(pop, emat, f)
 
     @test length(pop.seqs) == 1
     @test length(pop.seqs[1]) == 10
